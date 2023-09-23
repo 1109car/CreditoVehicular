f8ba9955ff0165f155ad5b2929f0d4d90657e7a0
carlos <crlos.rjas.2001@gmail.com>
2023-09-23 01:48:49 -0500

    carlos

---
+++ app/cuotas/pag.jsx
@@ -0,0 +1,12 @@
+
+import Page from "../cuotas/page.jsx"
+export default function pag() {
+  return (
+    <>
+      <Page
+      oferta={"OFERTAS"}
+      fecha={"Dia de pago"}
+      />
+    </>
+  )
+}
